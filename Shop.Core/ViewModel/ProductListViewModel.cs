4f07f2aad16dd3dc66993af972c72e5b0bc84e8f
dorra <benhmidadorra1@gmail.com>
2021-01-20 11:37:32 +0100

    details add

---
+++ Shop.Core/ViewModel/ProductListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Shop.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Core.ViewModel
+{
+    public class ProductListViewModel
+    {
+        public List<Product> Products { get; set; }
+        public List<ProductCategory> ProductsCategory { get; set; }
+    }
+}
